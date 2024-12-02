aaf3a053154c871d7b2385593c84c4c262466871
MirthFutures <MirthFutures@gmail.com>
2024-12-02 14:50:38 -0500

    Init CAP

---
+++ test/Tes.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract Test is Test {
+    Counter public counter;
+
+    function setUp() public {
+        
+    }
+}
