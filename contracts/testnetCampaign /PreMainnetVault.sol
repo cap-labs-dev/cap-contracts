88da6e83aa723501787e506e8dfdfbac0b7d633f
MirthFutures <MirthFutures@gmail.com>
2025-01-11 10:36:19 -0500

    Add Pre Mainnet Vault

---
+++ contracts/testnetCampaign /PreMainnetVault.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title PreMainnetVault
+ * @notice Vault for pre-mainnet campaign
+ */
+contract PreMainnetVault is ERC20, Ownable {
+
+    /// @notice Deposit token most likely USDC
+    IERC20 public depositToken;
+
+    /// @notice Withdraw enabled flag after campaign ends
+    bool public withdrawEnabled;
+
+    error ZeroAmount();
+    error WithdrawNotEnabled();
+    error TransferNotEnabled();
+
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
+    event WithdrawEnabled();
+
+    constructor(address _depositToken) ERC20("Pre Mainnet Vault Token", "capPMV") Ownable(msg.sender) {
+        depositToken = IERC20(_depositToken);
+    }
+
+
+    /***
+     * @notice Deposit depositToken to mint cUSD on MegaETH Testnet
+     * @param _amount Amount of depositToken to deposit
+     */
+    function deposit(uint256 _amount) external {
+        if (_amount == 0) revert ZeroAmount();
+
+        depositToken.transferFrom(_msgSender(), address(this), _amount);
+
+        _mint(_msgSender(), _amount);
+
+        /// todo: lz bridge logic to mint on testnet 
+
+        emit Deposit(_msgSender(), _amount);
+    }
+
+    /***
+     * @notice Withdraw depositToken after campaign ends
+     * @param _amount Amount of depositToken to withdraw
+     */
+    function withdraw(uint256 _amount) external {
+        if (!withdrawEnabled) revert WithdrawNotEnabled();
+
+        _burn(_msgSender(), _amount);
+
+        depositToken.transfer(msg.sender, _amount);
+
+        emit Withdraw(_msgSender(), _amount);
+    }
+
+    /**
+     * @notice Enable withdraw after campaign ends
+     */
+    function enableWithdraw() external onlyOwner {
+        withdrawEnabled = true;
+
+        emit WithdrawEnabled();
+    }
+
+    /// @notice Token is soulbound until transfer is enabled
+    function transfer(address to, uint256 value) public virtual override returns (bool) {
+        if (!withdrawEnabled) revert TransferNotEnabled();
+        address owner = _msgSender();
+        _transfer(owner, to, value);
+        return true;
+    }
+
+    /// @notice Token is soulbound until transfer is enabled
+    function transferFrom(address from, address to, uint256 value) public virtual override returns (bool) {
+        if (!withdrawEnabled) revert TransferNotEnabled();
+        address spender = _msgSender();
+        _spendAllowance(from, spender, value);
+        _transfer(from, to, value);
+        return true;
+    }
+
+    function tvl() external view returns (uint256) {
+        return depositToken.balanceOf(address(this));
+    }
+}
