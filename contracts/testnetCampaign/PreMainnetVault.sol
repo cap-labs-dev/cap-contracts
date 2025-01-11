91c96da3cd69299c081e88f0802b4a9ee52b2d98
MirthFutures <MirthFutures@gmail.com>
2025-01-11 11:28:57 -0500

    Match decimals of deposit token

--- contracts/testnetCampaign/PreMainnetVault.sol
+++ contracts/testnetCampaign/PreMainnetVault.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.28;
 
 import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
@@ -12,6 +13,9 @@ contract PreMainnetVault is ERC20, Ownable {
 
     /// @notice Deposit token most likely USDC
     IERC20 public depositToken;
+
+    /// @notice Decimals of the token
+    uint8 private _decimals;
 
     /// @notice Withdraw enabled flag after campaign ends
     bool public withdrawEnabled;
@@ -26,6 +30,8 @@ contract PreMainnetVault is ERC20, Ownable {
 
     constructor(address _depositToken) ERC20("Pre Mainnet Vault Token", "capPMV") Ownable(msg.sender) {
         depositToken = IERC20(_depositToken);
+
+        _decimals = IERC20Metadata(_depositToken).decimals();
     }
 
 
@@ -66,6 +72,10 @@ contract PreMainnetVault is ERC20, Ownable {
         withdrawEnabled = true;
 
         emit WithdrawEnabled();
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
     }
 
     /// @notice Token is soulbound until transfer is enabled
