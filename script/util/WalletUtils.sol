465499d87c35421f2ad352dec629c57987cb7457
prevostc <998369+prevostc@users.noreply.github.com>
2025-01-14 02:08:54 +0100

    Add wallet utils

--- script/util/WalletUtils.sol
+++ script/util/WalletUtils.sol
@@ -2,18 +2,20 @@
 pragma solidity ^0.8.28;
 
 contract WalletUtils {
-    function getWalletAddress() public view returns (address owner) {
-        owner = tx.origin;
-        if (owner == address(0)) {
-            owner = msg.sender;
+    function getWalletAddress() public view returns (address wallet) {
+        wallet = tx.origin;
+        if (wallet == address(0)) {
+            wallet = msg.sender;
         }
 
-        if (owner == address(0)) {
-            revert("Owner not found");
+        if (wallet == address(0)) {
+            revert("Wallet address not set");
         }
 
-        if (owner == 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38) {
-            revert("Owner is set to the default foundry address");
+        if (wallet == 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38) {
+            revert(
+                "Wallet address is set to the default foundry address. Plz provide --sender $(cast wallet address --account cap-dev)"
+            );
         }
     }
 }
