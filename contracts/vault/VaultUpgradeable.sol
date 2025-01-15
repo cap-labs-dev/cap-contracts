3440aeaf801ea9847f7f6edcf353f85dd5f792b0
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2025-01-15 13:28:20 +0000

    Correct import

--- contracts/vault/VaultUpgradeable.sol
+++ contracts/vault/VaultUpgradeable.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.28;
 
 import { AccessUpgradeable } from "../registry/AccessUpgradeable.sol";
-import { MinterUpgradeable } from "../minter/MinterUpgradeable.sol";
+import { MinterUpgradeable } from "./MinterUpgradeable.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
