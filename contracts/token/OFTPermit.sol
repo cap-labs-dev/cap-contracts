8c5de36bee302ef62e8bbb18ff77c9bd1d570100
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2025-01-24 16:01:55 +0000

    rmv unused import & clean comments on lockbox

--- contracts/token/OFTPermit.sol
+++ contracts/token/OFTPermit.sol
@@ -7,18 +7,14 @@ import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC2
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IOFT, OFTCore } from "@layerzerolabs/oft-evm/contracts/OFTCore.sol";
 
-/**
- * @title OFT Permit Contract
- * @dev OFT is an ERC-20 Permit token that extends the functionality of the OFTCore contract.
- */
+/// @title OFT Permit Contract
+/// @dev OFT is an ERC-20 Permit token that extends the functionality of the OFTCore contract.
 abstract contract OFTPermit is OFTCore, ERC20, ERC20Permit {
-    /**
-     * @dev Constructor for the OFT contract.
-     * @param _name The name of the OFT.
-     * @param _symbol The symbol of the OFT.
-     * @param _lzEndpoint The LayerZero endpoint address.
-     * @param _delegate The delegate capable of making OApp configurations inside of the endpoint.
-     */
+    /// @dev Constructor for the OFT contract.
+    /// @param _name The name of the OFT.
+    /// @param _symbol The symbol of the OFT.
+    /// @param _lzEndpoint The LayerZero endpoint address.
+    /// @param _delegate The delegate capable of making OApp configurations inside of the endpoint.
     constructor(
         string memory _name,
         string memory _symbol,
@@ -26,35 +22,27 @@ abstract contract OFTPermit is OFTCore, ERC20, ERC20Permit {
         address _delegate
     ) ERC20(_name, _symbol) ERC20Permit(_name) OFTCore(decimals(), _lzEndpoint, _delegate) Ownable(_delegate) {}
 
-    /**
-     * @dev Retrieves the address of the underlying ERC20 implementation.
-     * @return The address of the OFT token.
-     *
-     * @dev In the case of OFT, address(this) and erc20 are the same contract.
-     */
+    /// @dev Retrieves the address of the underlying ERC20 implementation.
+    /// @return The address of the OFT token.
+    /// @dev In the case of OFT, address(this) and erc20 are the same contract.
     function token() public view returns (address) {
         return address(this);
     }
 
-    /**
-     * @notice Indicates whether the OFT contract requires approval of the 'token()' to send.
-     * @return requiresApproval Needs approval of the underlying token implementation.
-     *
-     * @dev In the case of OFT where the contract IS the token, approval is NOT required.
-     */
+    /// @notice Indicates whether the OFT contract requires approval of the 'token()' to send.
+    /// @return requiresApproval Needs approval of the underlying token implementation.
+    /// @dev In the case of OFT where the contract IS the token, approval is NOT required.
     function approvalRequired() external pure virtual returns (bool) {
         return false;
     }
 
-    /**
-     * @dev Burns tokens from the sender's specified balance.
-     * @param _from The address to debit the tokens from.
-     * @param _amountLD The amount of tokens to send in local decimals.
-     * @param _minAmountLD The minimum amount to send in local decimals.
-     * @param _dstEid The destination chain ID.
-     * @return amountSentLD The amount sent in local decimals.
-     * @return amountReceivedLD The amount received in local decimals on the remote.
-     */
+    /// @dev Burns tokens from the sender's specified balance.
+    /// @param _from The address to debit the tokens from.
+    /// @param _amountLD The amount of tokens to send in local decimals.
+    /// @param _minAmountLD The minimum amount to send in local decimals.
+    /// @param _dstEid The destination chain ID.
+    /// @return amountSentLD The amount sent in local decimals.
+    /// @return amountReceivedLD The amount received in local decimals on the remote.
     function _debit(
         address _from,
         uint256 _amountLD,
@@ -70,13 +58,11 @@ abstract contract OFTPermit is OFTCore, ERC20, ERC20Permit {
         _burn(_from, amountSentLD);
     }
 
-    /**
-     * @dev Credits tokens to the specified address.
-     * @param _to The address to credit the tokens to.
-     * @param _amountLD The amount of tokens to credit in local decimals.
-     * @dev _srcEid The source chain ID.
-     * @return amountReceivedLD The amount of tokens ACTUALLY received in local decimals.
-     */
+    /// @dev Credits tokens to the specified address.
+    /// @param _to The address to credit the tokens to.
+    /// @param _amountLD The amount of tokens to credit in local decimals.
+    /// @dev _srcEid The source chain ID.
+    /// @return amountReceivedLD The amount of tokens ACTUALLY received in local decimals.
     function _credit(
         address _to,
         uint256 _amountLD,
