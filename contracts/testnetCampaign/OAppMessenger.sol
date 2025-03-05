7e2837ac8a726862f21e6a85ecf4354b82ccc08d
prevostc <998369+prevostc@users.noreply.github.com>
2025-03-05 16:07:53 +0100

    Parameter _gas can be removed #4

--- contracts/testnetCampaign/OAppMessenger.sol
+++ contracts/testnetCampaign/OAppMessenger.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
-import { OAppSender, OAppCore } from "@layerzerolabs/oapp-evm/contracts/oapp/OAppSender.sol";
+import { OAppCore, OAppSender } from "@layerzerolabs/oapp-evm/contracts/oapp/OAppSender.sol";
 import { OptionsBuilder } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
 import { MessagingFee, OFTReceipt } from "@layerzerolabs/oft-evm/contracts/interfaces/IOFT.sol";
 import { OFTMsgCodec } from "@layerzerolabs/oft-evm/contracts/libs/OFTMsgCodec.sol";
@@ -32,7 +32,7 @@ abstract contract OAppMessenger is OAppSender {
     /// @param _destReceiver Receiver of the assets on MegaETH Testnet
     /// @return fee Fee for the LayerZero bridge
     function quote(uint256 _amountLD, address _destReceiver) external view returns (MessagingFee memory fee) {
-        (bytes memory message, bytes memory options) = _buildMsgAndOptions(lzReceiveGas, _amountLD, _destReceiver);
+        (bytes memory message, bytes memory options) = _buildMsgAndOptions(_amountLD, _destReceiver);
         fee = _quote(dstEid, message, options, false);
     }
 
@@ -41,23 +41,22 @@ abstract contract OAppMessenger is OAppSender {
     /// @param _amountLD Amount of asset in local decimals
     function _sendMessage(address _destReceiver, uint256 _amountLD) internal {
         MessagingFee memory _fee = MessagingFee({ nativeFee: msg.value, lzTokenFee: 0 });
-        (bytes memory message, bytes memory options) = _buildMsgAndOptions(lzReceiveGas, _amountLD, _destReceiver);
+        (bytes memory message, bytes memory options) = _buildMsgAndOptions(_amountLD, _destReceiver);
         _lzSend(dstEid, message, options, _fee, msg.sender);
     }
 
     /// @dev Build the message and options for the LayerZero bridge
-    /// @param _gas Gas fee
     /// @param _amountLD Amount in local decimals
     /// @param _destReceiver Receiver of the assets on MegaETH Testnet
     /// @return message Message for the LayerZero bridge
     /// @return options Options for the LayerZero bridge
-    function _buildMsgAndOptions(uint128 _gas, uint256 _amountLD, address _destReceiver)
+    function _buildMsgAndOptions(uint256 _amountLD, address _destReceiver)
         internal
         view
         returns (bytes memory message, bytes memory options)
     {
         (message,) = OFTMsgCodec.encode(OFTMsgCodec.addressToBytes32(_destReceiver), _toSD(_amountLD), "");
-        options = OptionsBuilder.newOptions().addExecutorLzReceiveOption(_gas, 0);
+        options = OptionsBuilder.newOptions().addExecutorLzReceiveOption(lzReceiveGas, 0);
     }
 
     /// @dev Convert amount in shared decimals to amount in local decimals
