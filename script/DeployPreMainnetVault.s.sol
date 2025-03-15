256e06194cef9fb8e148e576d8ffdc7ece8edf6d
prevostc <998369+prevostc@users.noreply.github.com>
2025-03-15 23:54:42 +0100

    Add pm vault deploy script

--- script/DeployPreMainnetVault.s.sol
+++ script/DeployPreMainnetVault.s.sol
@@ -10,10 +10,9 @@ import { WalletUtils } from "../contracts/deploy/utils/WalletUtils.sol";
 import { PreMainnetVault } from "../contracts/testnetCampaign/PreMainnetVault.sol";
 import { L2Token } from "../contracts/token/L2Token.sol";
 
-contract DeployPreMainnetVaultAndL2Token is Script, LzUtils, WalletUtils {
+contract DeployPreMainnetVault is Script, LzUtils, WalletUtils {
     using stdJson for string;
 
-    // Chain IDs
     string constant SOURCE_RPC_URL = "mainnet";
     uint256 constant SOURCE_CHAIN_ID = 1;
 
@@ -21,13 +20,14 @@ contract DeployPreMainnetVaultAndL2Token is Script, LzUtils, WalletUtils {
     uint256 constant TARGET_CHAIN_ID = 6342;
 
     // Max campaign length (1 week)
-    uint48 constant MAX_CAMPAIGN_LENGTH = 7 days;
+    uint48 constant CAMPAIGN_TIMESTAMP_END = 1754006400;
 
     uint256 mainnetForkId;
     uint256 megaethTestnetForkId;
 
     // Deployment addresses
     address public usdc;
+    address public l2TokenAddress;
 
     // Deployed contracts
     PreMainnetVault public vault;
@@ -38,48 +38,41 @@ contract DeployPreMainnetVaultAndL2Token is Script, LzUtils, WalletUtils {
     LzAddressbook public megaethTestnetConfig;
 
     function run() external {
-        address deployer = getWalletAddress();
+        // address deployer = getWalletAddress();
         mainnetForkId = vm.createFork(SOURCE_RPC_URL);
-        megaethTestnetForkId = vm.createFork(TARGET_RPC_URL);
+        // megaethTestnetForkId = vm.createFork(TARGET_RPC_URL);
 
         // Get deployment configuration
         usdc = vm.envAddress("USDC_ADDRESS");
+        l2TokenAddress = vm.envAddress("L2TOKEN_ADDRESS");
 
         // Get LayerZero configuration for both chains
         mainnetConfig = _getLzAddressbook(SOURCE_CHAIN_ID);
         megaethTestnetConfig = _getLzAddressbook(TARGET_CHAIN_ID);
 
+        // compute time left in campaign
+        console.log("current timestamp:", block.timestamp);
+        console.log("campaign timestamp end:", CAMPAIGN_TIMESTAMP_END);
+        uint48 timeLeft = uint48(CAMPAIGN_TIMESTAMP_END - block.timestamp);
+        console.log("time left:", timeLeft);
+
         // Deploy PreMainnetVault on Sepolia
         vm.selectFork(mainnetForkId);
         vm.startBroadcast();
-        vault =
-            new PreMainnetVault(usdc, address(mainnetConfig.endpointV2), megaethTestnetConfig.eid, MAX_CAMPAIGN_LENGTH);
+        vault = new PreMainnetVault(usdc, address(mainnetConfig.endpointV2), megaethTestnetConfig.eid, timeLeft);
         console.log("PreMainnetVault deployed on Sepolia at:", address(vault));
         vm.stopBroadcast();
 
-        // Deploy L2Token on Arbitrum Sepolia
-        vm.selectFork(megaethTestnetForkId);
-        vm.startBroadcast();
-        l2Token = new L2Token("Boosted cUSD", "bcUSD", address(megaethTestnetConfig.endpointV2), deployer);
-        console.log("L2Token deployed on Arbitrum Sepolia at:", address(l2Token));
-        vm.stopBroadcast();
+        l2Token = L2Token(l2TokenAddress);
 
         // Link the contracts
         bytes32 l2TokenPeer = addressToBytes32(address(l2Token));
-        bytes32 vaultPeer = addressToBytes32(address(vault));
 
         // Set PreMainnetVault's peer to L2Token
         vm.selectFork(mainnetForkId);
         vm.startBroadcast();
         vault.setPeer(megaethTestnetConfig.eid, l2TokenPeer);
         console.log("Set PreMainnetVault's peer to L2Token");
-        vm.stopBroadcast();
-
-        // Set L2Token's peer to PreMainnetVault
-        vm.selectFork(megaethTestnetForkId);
-        vm.startBroadcast();
-        l2Token.setPeer(mainnetConfig.eid, vaultPeer);
-        console.log("Set L2Token's peer to PreMainnetVault");
         vm.stopBroadcast();
     }
 }
