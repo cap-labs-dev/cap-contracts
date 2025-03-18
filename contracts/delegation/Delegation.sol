81a1a9e6759b7cf7c1c7b827031e522bf7942651
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2025-03-18 15:57:58 -0400

    Zellic H1 (#81)
    https://github.com/cap-labs-dev/cap-contracts/pull/81/#issue-2929046434

--- contracts/delegation/Delegation.sol
+++ contracts/delegation/Delegation.sol
@@ -20,9 +20,6 @@ import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 contract Delegation is IDelegation, UUPSUpgradeable, Access, DelegationStorageUtils {
     using SafeERC20 for IERC20;
 
-    /// @notice Invalid liquidation threshold
-    error InvalidLiquidationThreshold();
-
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -38,6 +35,7 @@ contract Delegation is IDelegation, UUPSUpgradeable, Access, DelegationStorageUt
         DelegationStorage storage $ = getDelegationStorage();
         $.oracle = _oracle;
         $.epochDuration = _epochDuration;
+        $.ltvBuffer = 0.05e27; // 5%
     }
 
     /// @notice How much global delegation we have in the system
@@ -61,6 +59,12 @@ contract Delegation is IDelegation, UUPSUpgradeable, Access, DelegationStorageUt
     function epoch() public view returns (uint256 currentEpoch) {
         DelegationStorage storage $ = getDelegationStorage();
         currentEpoch = block.timestamp / $.epochDuration;
+    }
+
+    /// @notice Get the ltv buffer
+    /// @return buffer LTV buffer
+    function ltvBuffer() external view returns (uint256 buffer) {
+        buffer = getDelegationStorage().ltvBuffer;
     }
 
     /// @notice Get the timestamp that is most recent between the last borrow and the epoch -1
@@ -206,11 +210,11 @@ contract Delegation is IDelegation, UUPSUpgradeable, Access, DelegationStorageUt
         external
         checkAccess(this.addAgent.selector)
     {
-        // if liquidation threshold is greater than 100%, agent
-        // could borrow more than they are collateralized for
-        if (_liquidationThreshold > 1e27) revert InvalidLiquidationThreshold();
-
         DelegationStorage storage $ = getDelegationStorage();
+
+        // if ltv is greater than 100% then agent could borrow more than they are collateralized for
+        if (_liquidationThreshold > 1e27) revert InvalidLiquidationThreshold();
+        if (_ltv != 0 && _liquidationThreshold < _ltv + $.ltvBuffer) revert LiquidationThresholdTooCloseToLtv();
 
         // If the agent already exists, we revert
         for (uint i; i < $.agents.length; ++i) {
@@ -232,6 +236,10 @@ contract Delegation is IDelegation, UUPSUpgradeable, Access, DelegationStorageUt
         checkAccess(this.modifyAgent.selector)
     {
         DelegationStorage storage $ = getDelegationStorage();
+
+        // if ltv is greater than 100% then agent could borrow more than they are collateralized for
+        if (_liquidationThreshold > 1e27) revert InvalidLiquidationThreshold();
+        if (_ltv != 0 && _liquidationThreshold < _ltv + $.ltvBuffer) revert LiquidationThresholdTooCloseToLtv();
 
         // Check that the agent exists
         for (uint i; i < $.agents.length; ++i) {
@@ -259,6 +267,14 @@ contract Delegation is IDelegation, UUPSUpgradeable, Access, DelegationStorageUt
         $.networks[_agent].push(_network);
 
         emit RegisterNetwork(_agent, _network);
+    }
+
+    /// @notice Set the ltv buffer
+    /// @param _ltvBuffer LTV buffer
+    function setLtvBuffer(uint256 _ltvBuffer) external checkAccess(this.setLtvBuffer.selector) {
+        if (_ltvBuffer > 1e27) revert InvalidLtvBuffer();
+        getDelegationStorage().ltvBuffer = _ltvBuffer;
+        emit SetLtvBuffer(_ltvBuffer);
     }
 
     /// @dev Only admin can upgrade
