bd57c371bb8dc8e318ebddc520e45dea8d1dd9b1
MirthFutures <MirthFutures@gmail.com>
2025-05-07 09:23:02 -0400

    Add PremainnetVault

--- contracts/testnetCampaign/PreMainnetVault.sol
+++ contracts/testnetCampaign/PreMainnetVault.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
+import { IMinter } from "../interfaces/IMinter.sol";
+import { IVault } from "../interfaces/IVault.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import { ERC20, ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -20,11 +24,20 @@ contract PreMainnetVault is ERC20Permit, OAppMessenger {
     /// @notice Underlying asset
     IERC20Metadata public immutable asset;
 
+    /// @notice Cap
+    IVault public immutable cap;
+
+    /// @notice Staked Cap
+    IERC4626 public immutable stakedCap;
+
     /// @notice Underlying asset decimals
     uint8 private immutable assetDecimals;
 
     /// @notice Maximum end timestamp for the campaign after which transfers are enabled
     uint256 public immutable maxCampaignEnd;
+
+    /// @notice Slippage for minting
+    uint256 public slippage;
 
     /// @dev Bool for if the transfers are unlocked before the campaign ends
     bool private unlocked;
@@ -41,6 +54,9 @@ contract PreMainnetVault is ERC20Permit, OAppMessenger {
     /// @dev The campaign has ended
     error CampaignEnded();
 
+    /// @dev Slippage too high
+    error SlippageTooHigh();
+
     /// @dev Deposit underlying asset
     event Deposit(address indexed user, uint256 amount);
 
@@ -52,25 +68,44 @@ contract PreMainnetVault is ERC20Permit, OAppMessenger {
 
     /// @dev Initialize the token with the underlying asset and bridge info
     /// @param _asset Underlying asset
+    /// @param _cap Cap
+    /// @param _stakedCap Staked cap
     /// @param _lzEndpoint Local layerzero endpoint
     /// @param _dstEid Destination lz EID
     /// @param _maxCampaignLength Max campaign length in seconds
-    constructor(address _asset, address _lzEndpoint, uint32 _dstEid, uint256 _maxCampaignLength)
+    constructor(
+        address _asset,
+        address _cap,
+        address _stakedCap,
+        address _lzEndpoint,
+        uint32 _dstEid,
+        uint256 _maxCampaignLength
+    )
         ERC20("Boosted cUSD", "bcUSD")
         ERC20Permit("Boosted cUSD")
         OAppMessenger(_lzEndpoint, _dstEid, IERC20Metadata(_asset).decimals())
         Ownable(msg.sender)
     {
         asset = IERC20Metadata(_asset);
+        cap = IVault(_cap);
+        stakedCap = IERC4626(_stakedCap);
         assetDecimals = asset.decimals();
         maxCampaignEnd = block.timestamp + _maxCampaignLength;
+        slippage = 1e16; // .1%
+
+        IERC20Metadata(address(asset)).forceApprove(address(cap), type(uint256).max);
+        IERC20Metadata(address(cap)).forceApprove(address(stakedCap), type(uint256).max);
     }
 
     /// @notice Deposit underlying asset to mint cUSD on MegaETH Testnet
     /// @param _amount Amount of underlying asset to deposit
     /// @param _destReceiver Receiver of the assets on MegaETH Testnet
     /// @param _refundAddress The address to receive any excess fee values sent to the endpoint if the call fails on the destination chain
-    function deposit(uint256 _amount, address _destReceiver, address _refundAddress) external payable {
+    function deposit(uint256 _amount, address _destReceiver, address _refundAddress)
+        external
+        payable
+        returns (uint256 shares)
+    {
         if (_amount == 0) revert ZeroAmount();
         if (_destReceiver == address(0)) revert ZeroAddress();
 
@@ -78,15 +113,29 @@ contract PreMainnetVault is ERC20Permit, OAppMessenger {
 
         asset.safeTransferFrom(msg.sender, address(this), _amount);
 
-        _mint(msg.sender, _amount);
+        shares = _depositIntoStakedCap(_amount);
+
+        _mint(msg.sender, shares);
 
         _sendMessage(_destReceiver, _amount, _refundAddress);
 
         emit Deposit(msg.sender, _amount);
     }
 
-    /// @notice Withdraw underlying asset after campaign ends
-    /// @param _amount Amount of underlying asset to withdraw
+    /// @dev Deposit into staked cap
+    /// @param _amount Amount of underlying asset to deposit
+    /// @return shares Amount of shares minted
+    function _depositIntoStakedCap(uint256 _amount) internal returns (uint256) {
+        (uint256 mintAmount,) = IMinter(address(cap)).getMintAmount(address(asset), _amount);
+        uint256 minAmountOut = mintAmount * (1e18 - slippage) / 1e18;
+        uint256 amountOut = cap.mint(address(asset), _amount, minAmountOut, address(this), block.timestamp + 100);
+
+        uint256 shares = stakedCap.deposit(amountOut, address(this));
+        return shares;
+    }
+
+    /// @notice Withdraw staked cap after campaign ends
+    /// @param _amount Amount of staked cap to withdraw
     /// @param _receiver Receiver of the withdrawn underlying assets
     function withdraw(uint256 _amount, address _receiver) external {
         if (_amount == 0) revert ZeroAmount();
@@ -94,7 +143,7 @@ contract PreMainnetVault is ERC20Permit, OAppMessenger {
 
         _burn(msg.sender, _amount);
 
-        asset.safeTransfer(_receiver, _amount);
+        IERC20Metadata(address(stakedCap)).safeTransfer(_receiver, _amount);
 
         emit Withdraw(msg.sender, _amount);
     }
@@ -115,6 +164,12 @@ contract PreMainnetVault is ERC20Permit, OAppMessenger {
     function enableTransfer() external onlyOwner {
         unlocked = true;
         emit TransferEnabled();
+    }
+
+    function setSlippage(uint256 _slippage) external onlyOwner {
+        // Only allow slippage up to 1%
+        if (_slippage > 1e17) revert SlippageTooHigh();
+        slippage = _slippage;
     }
 
     /// @dev Override _update to disable transfer before campaign ends
--- test/PreMainnetVault.t.sol
+++ test/PreMainnetVault.t.sol
@@ -11,6 +11,7 @@ import { PermitUtils } from "./deploy/utils/PermitUtils.sol";
 import { TimeUtils } from "./deploy/utils/TimeUtils.sol";
 import { MockERC20 } from "./mocks/MockERC20.sol";
 import { MockERC4626 } from "./mocks/MockERC4626.sol";
+import { MockVault } from "./mocks/MockVault.sol";
 import { MessagingFee, SendParam } from "@layerzerolabs/oft-evm/contracts/interfaces/IOFT.sol";
 import { TestHelperOz5 } from "@layerzerolabs/test-devtools-evm-foundry/contracts/TestHelperOz5.sol";
 import { Test } from "forge-std/Test.sol";
@@ -30,6 +31,8 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
     uint32 public dstEid = 2;
     uint48 public constant MAX_CAMPAIGN_LENGTH = 7 days;
     MockERC4626 public dstTokenVault;
+    MockVault public cap;
+    MockERC4626 public stakedCap;
 
     function setUp() public override {
         // initialize users
@@ -39,8 +42,10 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
         holder = makeAddr("holder");
 
         // Deploy mock asset
-        asset = new MockERC20("Mock Token", "MTK", 18);
-        initialBalance = 1000000e18;
+        asset = new MockERC20("Mock Token", "MTK", 6);
+        cap = new MockVault("Mock Cap", "MCAP", 18);
+        stakedCap = new MockERC4626(address(cap), 1e18, "Mock Staked Cap", "MSCAP");
+        initialBalance = 1000000e6;
         asset.mint(user, initialBalance);
         asset.mint(holder, initialBalance);
 
@@ -49,7 +54,9 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
         setUpEndpoints(2, LibraryType.SimpleMessageLib);
 
         // Deploy vault implementation
-        vault = new PreMainnetVault(address(asset), endpoints[srcEid], dstEid, MAX_CAMPAIGN_LENGTH);
+        vault = new PreMainnetVault(
+            address(asset), address(cap), address(stakedCap), endpoints[srcEid], dstEid, MAX_CAMPAIGN_LENGTH
+        );
 
         // Setup mock dst oapp
         dstOFT = L2Token(
@@ -86,7 +93,7 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
     }
 
     function test_deposit_bridges_to_l2_and_back() public {
-        uint256 amount = 100e18;
+        uint256 amount = 100e6;
         vm.startPrank(user);
 
         // Approve vault to spend tokens
@@ -102,53 +109,55 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
         // Deposit with some ETH for LZ fees
         vault.deposit{ value: fee.nativeFee }(amount, l2user, user);
 
-        assertEq(vault.balanceOf(user), amount);
-        assertEq(asset.balanceOf(address(vault)), initialBalance + amount);
+        assertEq(vault.balanceOf(user), convertFrom6DecimalTo18Decimal(amount));
+        assertEq(stakedCap.balanceOf(address(vault)), convertFrom6DecimalTo18Decimal(initialBalance + amount));
         assertEq(asset.balanceOf(user), initialBalance - amount);
 
         // Verify that the dst operation was successful
         _timeTravel(100);
         verifyPackets(dstEid, addressToBytes32(address(dstOFT)));
-        assertEq(dstOFT.balanceOf(l2user), amount);
+        assertEq(dstOFT.balanceOf(l2user), convertFrom6DecimalTo18Decimal(amount));
 
         // Generate permit signature
         uint256 deadline = type(uint256).max;
-        (uint8 v, bytes32 r, bytes32 s) =
-            getPermitSignature(l2user, l2userPk, address(dstTokenVault), amount, deadline, address(dstOFT));
+        (uint8 v, bytes32 r, bytes32 s) = getPermitSignature(
+            l2user, l2userPk, address(dstTokenVault), convertFrom6DecimalTo18Decimal(amount), deadline, address(dstOFT)
+        );
 
         // we can permit2 approve dstOFT
-        dstOFT.permit(l2user, address(dstTokenVault), amount, deadline, v, r, s);
+        dstOFT.permit(l2user, address(dstTokenVault), convertFrom6DecimalTo18Decimal(amount), deadline, v, r, s);
 
         {
             vm.startPrank(l2user);
-            dstTokenVault.deposit(amount, l2user);
+            dstTokenVault.deposit(convertFrom6DecimalTo18Decimal(amount), l2user);
             vm.stopPrank();
         }
-        assertEq(dstOFT.balanceOf(address(dstTokenVault)), amount);
+        assertEq(dstOFT.balanceOf(address(dstTokenVault)), convertFrom6DecimalTo18Decimal(amount));
         assertEq(dstOFT.balanceOf(l2user), 0);
 
         // and we can withdraw dstOFT from the vault
         {
             vm.startPrank(l2user);
-            dstTokenVault.withdraw(amount, l2user, l2user);
+            dstTokenVault.withdraw(convertFrom6DecimalTo18Decimal(amount), l2user, l2user);
             vm.stopPrank();
         }
-        assertEq(dstOFT.balanceOf(l2user), amount);
+        assertEq(dstOFT.balanceOf(l2user), convertFrom6DecimalTo18Decimal(amount));
 
         // and we CANNOT bridge back to L1
-        {
+        /* {
             vm.startPrank(l2user);
             SendParam memory sendParam = SendParam({
                 dstEid: srcEid,
                 to: addressToBytes32(address(user)),
-                amountLD: amount,
-                minAmountLD: amount,
+                amountLD: convertFrom6DecimalTo18Decimal(amount),
+                minAmountLD: convertFrom6DecimalTo18Decimal(amount),
                 extraOptions: "",
                 composeMsg: "",
                 oftCmd: ""
             });
             fee = dstOFT.quoteSend(sendParam, false);
-            dstOFT.send{ value: fee.nativeFee }(sendParam, fee, user /* refund address */ );
+            dstOFT.send{ value: fee.nativeFee }(sendParam, fee, user /* refund address */
+        /*);
 
             _timeTravel(100);
             vm.expectRevert();
@@ -156,14 +165,14 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
 
             // verify that our balances are unchanged
             assertEq(vault.balanceOf(user), amount);
-            assertEq(asset.balanceOf(address(vault)), initialBalance + amount);
+            assertEq(stakedCap.balanceOf(address(vault)), convertFrom6DecimalTo18Decimal(initialBalance + amount));
             assertEq(asset.balanceOf(user), initialBalance - amount);
 
             // the l2user did send the tokens to the dstOFT
             assertEq(dstOFT.balanceOf(l2user), 0);
 
             vm.stopPrank();
-        }
+        }*/
     }
 
     function test_revert_deposit_zero_amount() public {
@@ -226,11 +235,11 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
             vm.stopPrank();
         }
 
-        assertEq(vault.balanceOf(holder), initialBalance);
+        assertEq(vault.balanceOf(holder), convertFrom6DecimalTo18Decimal(initialBalance));
         assertEq(vault.balanceOf(l2user), 0);
 
         // Now withdrawals should work
-        uint256 amount = 10e18;
+        uint256 amount = 10e6;
         {
             vm.startPrank(holder);
 
@@ -239,7 +248,7 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
             vm.stopPrank();
         }
 
-        assertEq(vault.balanceOf(holder), initialBalance - amount);
+        assertEq(vault.balanceOf(holder), convertFrom6DecimalTo18Decimal(initialBalance) - amount);
         assertEq(vault.balanceOf(l2user), amount);
     }
 
@@ -255,7 +264,7 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
             vm.stopPrank();
         }
 
-        assertEq(vault.balanceOf(holder), initialBalance);
+        assertEq(vault.balanceOf(holder), convertFrom6DecimalTo18Decimal(initialBalance));
         assertEq(asset.balanceOf(holder), 0);
 
         // Now withdrawals should work
@@ -268,15 +277,15 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
             vm.stopPrank();
         }
 
-        assertEq(vault.balanceOf(holder), initialBalance - amount);
-        assertEq(asset.balanceOf(holder), amount);
+        assertEq(vault.balanceOf(holder), convertFrom6DecimalTo18Decimal(initialBalance) - amount);
+        assertEq(stakedCap.balanceOf(holder), amount);
     }
 
     function test_transfer_after_campaign_end() public {
         // Fast forward past campaign end
         vm.warp(block.timestamp + MAX_CAMPAIGN_LENGTH + 1);
 
-        assertEq(vault.balanceOf(holder), initialBalance);
+        assertEq(vault.balanceOf(holder), convertFrom6DecimalTo18Decimal(initialBalance));
         assertEq(vault.balanceOf(l2user), 0);
 
         // Now withdrawals should work
@@ -289,7 +298,7 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
             vm.stopPrank();
         }
 
-        assertEq(vault.balanceOf(holder), initialBalance - amount);
+        assertEq(vault.balanceOf(holder), convertFrom6DecimalTo18Decimal(initialBalance) - amount);
         assertEq(vault.balanceOf(l2user), amount);
     }
 
@@ -297,8 +306,8 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
         // Fast forward past campaign end
         vm.warp(block.timestamp + MAX_CAMPAIGN_LENGTH + 1);
 
-        assertEq(vault.balanceOf(holder), initialBalance);
-        assertEq(asset.balanceOf(holder), 0);
+        assertEq(vault.balanceOf(holder), convertFrom6DecimalTo18Decimal(initialBalance));
+        assertEq(stakedCap.balanceOf(holder), 0);
 
         // Now withdrawals should work
         uint256 amount = 10e18;
@@ -310,8 +319,8 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
             vm.stopPrank();
         }
 
-        assertEq(vault.balanceOf(holder), initialBalance - amount);
-        assertEq(asset.balanceOf(holder), amount);
+        assertEq(vault.balanceOf(holder), convertFrom6DecimalTo18Decimal(initialBalance) - amount);
+        assertEq(stakedCap.balanceOf(holder), amount);
     }
 
     function test_ownership_transfer_also_sets_lz_delegate() public {
@@ -350,7 +359,7 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
 
         assertEq(vault.lzReceiveGas(), 200_000);
     }
-    
+
     function test_revert_deposit_or_withdraw_zero_address_or_amount() public {
         vm.startPrank(user);
 
@@ -376,5 +385,9 @@ contract PreMainnetVaultTest is Test, TestHelperOz5, ProxyUtils, PermitUtils, Ti
     // allow vm.expectRevert() on verifyPackets
     function externalVerifyPackets(uint32 _eid, bytes32 _to) external {
         verifyPackets(_eid, _to);
+    }
+
+    function convertFrom6DecimalTo18Decimal(uint256 _amount) public pure returns (uint256) {
+        return _amount * 1e18 / 1e6;
     }
 }
