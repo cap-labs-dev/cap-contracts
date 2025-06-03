503543f774e4254cecf025a0332396efc3cec7b0
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-06-03 19:09:50 -0300

    feat: global properties

--- test/recon/Properties.sol
+++ test/recon/Properties.sol
@@ -1,7 +1,60 @@
 // SPDX-License-Identifier: GPL-2.0
 pragma solidity ^0.8.0;
 
-import { BeforeAfter } from "./BeforeAfter.sol";
 import { Asserts } from "@chimera/Asserts.sol";
+import { MockERC20 } from "@recon/MockERC20.sol";
 
-abstract contract Properties is BeforeAfter, Asserts { }
+import { IVault } from "contracts/interfaces/IVault.sol";
+
+import { BeforeAfter } from "./BeforeAfter.sol";
+
+abstract contract Properties is BeforeAfter, Asserts {
+    /// @dev Property: Sum of deposits is less than or equal to total supply
+    function property_sum_of_deposits() public {
+        address[] memory actors = _getActors();
+
+        uint256 sumAssets;
+        for (uint256 i; i < actors.length; ++i) {
+            sumAssets += capToken.balanceOf(actors[i]);
+        }
+        // include the CUSD minted as fees sent to the insurance fund
+        IVault vault = IVault(address(env.usdVault.capToken));
+        sumAssets += capToken.balanceOf(vault.insuranceFund());
+
+        uint256 totalSupply = capToken.totalSupply();
+        lte(sumAssets, totalSupply, "sum of deposits is > total supply");
+    }
+
+    /// @dev Property: Sum of withdrawals is less than or equal to total supply
+    function property_sum_of_withdrawals() public {
+        address[] memory actors = _getActors();
+
+        lte(ghostAmountOut, capToken.totalSupply(), "sum of withdrawals is > total supply");
+    }
+
+    /// @dev Property: totalSupplies for a given asset is always <= vault balance
+    function property_vault_solvency_assets() public {
+        IVault vault = IVault(address(env.usdVault.capToken));
+        address[] memory assets = vault.assets();
+
+        for (uint256 i; i < assets.length; ++i) {
+            uint256 totalSupplied = vault.totalSupplies(assets[i]);
+            uint256 totalBorrow = vault.totalBorrows(assets[i]);
+            uint256 vaultBalance = MockERC20(assets[i]).balanceOf(address(vault));
+            lte(totalSupplied, vaultBalance, "totalSupplies > vault balance");
+            lte(totalBorrow, vaultBalance, "totalBorrows < totalSupplies");
+        }
+    }
+
+    /// @dev Property: totalSupplies for a given asset is always >= totalBorrows
+    function property_vault_solvency_borrows() public {
+        IVault vault = IVault(address(env.usdVault.capToken));
+        address[] memory assets = vault.assets();
+
+        for (uint256 i; i < assets.length; ++i) {
+            uint256 totalSupplied = vault.totalSupplies(assets[i]);
+            uint256 totalBorrow = vault.totalBorrows(assets[i]);
+            gte(totalSupplied, totalBorrow, "totalSupplies < totalBorrows");
+        }
+    }
+}
