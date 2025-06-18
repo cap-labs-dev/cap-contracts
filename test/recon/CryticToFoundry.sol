37690ecce6e9e3809e2a12e6dc768cc5b73f7618
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-06-18 14:07:22 -0300

    chore: update latest repros

--- test/recon/CryticToFoundry.sol
+++ test/recon/CryticToFoundry.sol
@@ -23,28 +23,6 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
         // TODO: add failing property tests here for debugging
     }
 
-    // forge test --match-test test_capToken_redeem_clamped_6 -vvv
-    // NOTE: issue is because of implementation of ERC4626Tester, need to determine best way to fix behavior
-    // TODO: determine best way to fix ERC4626Tester behavior
-    // NOTE: this no longer fails with the check for user allowance included because it was reverting before the cUSD was burned for the user
-    function test_capToken_redeem_clamped_6() public {
-        capToken_mint_clamped(10000037441);
-
-        add_new_vault();
-
-        capToken_setFractionalReserveVault();
-
-        capToken_investAll();
-
-        // issue is because shares in withdraw calculated by previewWithdraw now get increased but user balance doesn't
-        // most likely fix would be to rebase shares for all users when unbacked shares are minted
-        // or just make it so that users can only withdraw up to the maxWithdraw amount
-        // mockERC4626Tester_mintUnbackedShares(100003377823040994724, 0x0000000000000000000000000000000000000000);
-        // mockERC4626Tester_simulateLoss(200);
-
-        capToken_redeem_clamped(1);
-    }
-
     /// === Newest Issues === ///
 
     // forge test --match-test test_lender_liquidate_0 -vvv
@@ -65,6 +43,7 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
 
     // forge test --match-test test_property_debt_token_balance_gte_total_vault_debt_1 -vvv
     // NOTE: DebtToken balance < total vault debt, this looks valid
+    // NOTE: something is weird about the borrowing amount being type(uint256).max
     function test_property_debt_token_balance_gte_total_vault_debt_1() public {
         capToken_mint_clamped(10000718111);
 
@@ -76,21 +55,10 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
 
         switchActor(1);
 
+        // borrowing type(uint256).max here
         lender_borrow_clamped(115792089237316195423570985008687907853269984665640564039457584007913129639935);
-
+        console2.log("uint256.max)", type(uint256).max);
         property_debt_token_balance_gte_total_vault_debt();
-    }
-
-    // forge test --match-test test_capToken_burn_clamped_4 -vvv
-    // NOTE: user received more than expected amount out, this looks valid
-    function test_capToken_burn_clamped_4() public {
-        capToken_mint_clamped(10000718111);
-
-        switchChainlinkOracle(2);
-
-        mockChainlinkPriceFeed_setLatestAnswer(56196342554784885);
-
-        capToken_burn_clamped(14217);
     }
 
     // forge test --match-test test_capToken_mint_clamped_6 -vvv
@@ -104,12 +72,13 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
     }
 
     // forge test --match-test test_lender_realizeRestakerInterest_8 -vvv
-    // NOTE: Make sure this property should hold: agent total debt should not change after realizeRestakerInterest
+    // NOTE: agent health changes after realizeRestakerInterest when it shouldn't, could it be because of borrowing max?
     function test_lender_realizeRestakerInterest_8() public {
         switch_asset(0);
 
         capToken_mint_clamped(100711969);
 
+        // borrowing type(uint256).max here
         lender_borrow_clamped(115792089237316195423570985008687907853269984665640564039457584007913129639935);
 
         oracle_setRestakerRate(0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496, 33056249739822063734181);
