7f5198fdb429e8542f4f4d97189bd3a541c80722
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-06-27 10:17:08 -0300

    fix: remove incorrectly specified property

--- test/recon/Properties.sol
+++ test/recon/Properties.sol
@@ -487,14 +487,6 @@ abstract contract Properties is BeforeAfter, Asserts {
         }
     }
 
-    /// @dev Property: if no agent is borrowing, the current utilization index should be 0
-    function property_no_agent_borrowing_current_utilization_rate_should_be_zero() public {
-        if (_checkNoAgentBorrowing(_getAsset())) {
-            uint256 currentUtilizationIndex = capToken.currentUtilizationIndex(_getAsset());
-            eq(currentUtilizationIndex, 0, "current utilization index should be 0");
-        }
-    }
-
     /// === Optimization Properties === ///
 
     /// @dev test for optimizing the difference when debt token supply > total vault debt
