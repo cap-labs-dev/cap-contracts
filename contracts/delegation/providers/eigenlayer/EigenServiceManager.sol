233508ea05c4f03bccffc0525f9b0cac2732fe5b
MirthFutures <MirthFutures@gmail.com>
2025-08-04 03:39:04 -0400

    Add rewards distribution

--- contracts/delegation/providers/eigenlayer/EigenServiceManager.sol
+++ contracts/delegation/providers/eigenlayer/EigenServiceManager.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.28;
 import { Access } from "../../../access/Access.sol";
 import { IEigenServiceManager } from "../../../interfaces/IEigenServiceManager.sol";
 import { EigenServiceManagerStorageUtils } from "../../../storage/EigenServiceManagerStorageUtils.sol";
+
+import { IRewardsCoordinator } from "./interfaces/IRewardsCoordinator.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { IERC20, SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IAVSDirectory } from "eigenlayer-contracts/src/contracts/interfaces/IAVSDirectory.sol";
-import { IDelegationManager, IStrategy } from "eigenlayer-contracts/src/contracts/interfaces/IDelegationManager.sol";
-import { IRewardsCoordinator } from "eigenlayer-contracts/src/contracts/interfaces/IRewardsCoordinator.sol";
-import { IRegistryCoordinator } from "eigenlayer/src/interfaces/IRegistryCoordinator.sol";
-import { IStakeRegistry } from "eigenlayer/src/interfaces/IStakeRegistry.sol";
+import { IDelegationManager } from "eigenlayer-contracts/src/contracts/interfaces/IDelegationManager.sol";
+import { IStakeRegistry } from "eigenlayer-middleware/src/interfaces/IStakeRegistry.sol";
 
 contract EigenServiceManager is IEigenServiceManager, UUPSUpgradeable, Access, EigenServiceManagerStorageUtils {
     using SafeERC20 for IERC20;
@@ -68,30 +68,54 @@ contract EigenServiceManager is IEigenServiceManager, UUPSUpgradeable, Access, E
         return totalSlashableShares;
     }*/
 
-    ///// TO DO
     /// @inheritdoc IEigenServiceManager
-    /*function createAVSRewardsSubmission(
-        IRewardsCoordinator.RewardsSubmission[] calldata rewardsSubmissions
-    ) external checkAccess(this.createAVSRewardsSubmission.selector) {
+    function distributeRewards(address _agent, address _token) external checkAccess(this.distributeRewards.selector) {
         EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
-        for (uint256 i = 0; i < rewardsSubmissions.length; ++i) {
-            // transfer token to ServiceManager and approve RewardsCoordinator to transfer again
-            // in createAVSRewardsSubmission() call
-            rewardsSubmissions[i].token.safeTransferFrom(
-                msg.sender, address(this), rewardsSubmissions[i].amount
-            );
-            rewardsSubmissions[i].token.safeIncreaseAllowance(
-                address($.rewardsCoordinator), rewardsSubmissions[i].amount
-            );
+        uint256 _amount = IERC20(_token).balanceOf(address(this));
+        address _strategy = $.agentToStrategy[_agent];
+
+        IRewardsCoordinator.RewardsSubmission[] memory rewardsSubmissions =
+            new IRewardsCoordinator.RewardsSubmission[](1);
+        IRewardsCoordinator.StrategyAndMultiplier[] memory _strategiesAndMultipliers =
+            new IRewardsCoordinator.StrategyAndMultiplier[](1);
+        _strategiesAndMultipliers[0] =
+            IRewardsCoordinator.StrategyAndMultiplier({ strategy: _strategy, multiplier: 1e18 });
+
+        rewardsSubmissions[0] = IRewardsCoordinator.RewardsSubmission({
+            strategiesAndMultipliers: _strategiesAndMultipliers,
+            token: _token,
+            amount: _amount,
+            startTimestamp: uint32(block.timestamp),
+            duration: 0
+        });
+
+        _createAVSRewardsSubmission(rewardsSubmissions);
+    }
+
+    /// @notice Create a rewards submission for the AVS
+    /// @param rewardsSubmissions The rewards submissions being created
+    function _createAVSRewardsSubmission(IRewardsCoordinator.RewardsSubmission[] memory rewardsSubmissions) internal {
+        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
+        for (uint256 i; i < rewardsSubmissions.length; ++i) {
+            _checkApproval(rewardsSubmissions[i].token, $.rewardsCoordinator);
         }
 
-        $.rewardsCoordinator.createAVSRewardsSubmission(rewardsSubmissions);
-    }*/
+        IRewardsCoordinator($.rewardsCoordinator).createAVSRewardsSubmission(rewardsSubmissions);
+    }
 
     /// @inheritdoc IEigenServiceManager
     function avsDirectory() external view override returns (address) {
         EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
         return $.avsDirectory;
+    }
+
+    /// @notice Check if the token has enough allowance for the spender
+    /// @param token The token to check
+    /// @param spender The spender to check
+    function _checkApproval(address token, address spender) internal {
+        if (IERC20(token).allowance(spender, address(this)) == 0) {
+            IERC20(token).forceApprove(spender, type(uint256).max);
+        }
     }
 
     /// @inheritdoc UUPSUpgradeable
---
+++ contracts/delegation/providers/eigenlayer/interfaces/IRewardsCoordinator.sol
@@ -0,0 +1,63 @@
+    // SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.27;
+
+interface IRewardsCoordinator {
+    /**
+     * @notice A linear combination of strategies and multipliers for AVSs to weigh
+     * EigenLayer strategies.
+     * @param strategy The EigenLayer strategy to be used for the rewards submission
+     * @param multiplier The weight of the strategy in the rewards submission
+     */
+    struct StrategyAndMultiplier {
+        address strategy;
+        uint96 multiplier;
+    }
+
+    /**
+     * Sliding Window for valid RewardsSubmission startTimestamp
+     *
+     * Scenario A: GENESIS_REWARDS_TIMESTAMP IS WITHIN RANGE
+     *         <-----MAX_RETROACTIVE_LENGTH-----> t (block.timestamp) <---MAX_FUTURE_LENGTH--->
+     *             <--------------------valid range for startTimestamp------------------------>
+     *             ^
+     *         GENESIS_REWARDS_TIMESTAMP
+     *
+     *
+     * Scenario B: GENESIS_REWARDS_TIMESTAMP IS OUT OF RANGE
+     *         <-----MAX_RETROACTIVE_LENGTH-----> t (block.timestamp) <---MAX_FUTURE_LENGTH--->
+     *         <------------------------valid range for startTimestamp------------------------>
+     *     ^
+     * GENESIS_REWARDS_TIMESTAMP
+     * @notice RewardsSubmission struct submitted by AVSs when making rewards for their operators and stakers
+     * RewardsSubmission can be for a time range within the valid window for startTimestamp and must be within max duration.
+     * See `createAVSRewardsSubmission()` for more details.
+     * @param strategiesAndMultipliers The strategies and their relative weights
+     * cannot have duplicate strategies and need to be sorted in ascending address order
+     * @param token The rewards token to be distributed
+     * @param amount The total amount of tokens to be distributed
+     * @param startTimestamp The timestamp (seconds) at which the submission range is considered for distribution
+     * could start in the past or in the future but within a valid range. See the diagram above.
+     * @param duration The duration of the submission range in seconds. Must be <= MAX_REWARDS_DURATION
+     */
+    struct RewardsSubmission {
+        StrategyAndMultiplier[] strategiesAndMultipliers;
+        address token;
+        uint256 amount;
+        uint32 startTimestamp;
+        uint32 duration;
+    }
+
+    /**
+     * @notice Creates a new rewards submission on behalf of an AVS, to be split amongst the
+     * set of stakers delegated to operators who are registered to the `avs`
+     * @param rewardsSubmissions The rewards submissions being created
+     * @dev Expected to be called by the ServiceManager of the AVS on behalf of which the submission is being made
+     * @dev The duration of the `rewardsSubmission` cannot exceed `MAX_REWARDS_DURATION`
+     * @dev The duration of the `rewardsSubmission` cannot be 0 and must be a multiple of `CALCULATION_INTERVAL_SECONDS`
+     * @dev The tokens are sent to the `RewardsCoordinator` contract
+     * @dev Strategies must be in ascending order of addresses to check for duplicates
+     * @dev This function will revert if the `rewardsSubmission` is malformed,
+     * e.g. if the `strategies` and `weights` arrays are of non-equal lengths
+     */
+    function createAVSRewardsSubmission(RewardsSubmission[] calldata rewardsSubmissions) external;
+}
