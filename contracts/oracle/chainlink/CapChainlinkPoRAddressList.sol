41ef7e22be6b6e095d1c25668c928d55fb71147d
MirthFutures <MirthFutures@gmail.com>
2025-08-12 03:55:44 -0400

    Update Cap Chainlink PoRList

--- contracts/oracle/chainlink/CapChainlinkPoRAddressList.sol
+++ contracts/oracle/chainlink/CapChainlinkPoRAddressList.sol
@@ -1,18 +1,33 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
+import { Access } from "../../access/Access.sol";
 import { ICapChainlinkPoRAddressList } from "../../interfaces/ICapChainlinkPoRAddressList.sol";
-
 import { IVault } from "../../interfaces/IVault.sol";
 import { CapChainlinkPoRAddressListStorageUtils } from "../../storage/CapChainlinkPoRAddressListStorageUtils.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 /// @title Chainlink PoR Address List
 /// @author weso, Cap Labs
 /// @dev This contract is used to store the list of addresses that are used to verify the proof of reserves for cUSD
-contract CapChainlinkPoRAddressList is ICapChainlinkPoRAddressList, CapChainlinkPoRAddressListStorageUtils {
+contract CapChainlinkPoRAddressList is
+    ICapChainlinkPoRAddressList,
+    UUPSUpgradeable,
+    Access,
+    CapChainlinkPoRAddressListStorageUtils
+{
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    /// @param _accessControl The address of the access control contract
     /// @param _cusd The address of the cUSD vault
-    constructor(address _cusd) {
+    function initialize(address _accessControl, address _cusd) public initializer {
         CapChainlinkPoRAddressListStorage storage $ = getCapChainlinkPoRAddressListStorage();
+        __Access_init(_accessControl);
+        __UUPSUpgradeable_init();
         $.cusd = IVault(_cusd);
     }
 
@@ -22,22 +37,41 @@ contract CapChainlinkPoRAddressList is ICapChainlinkPoRAddressList, CapChainlink
     }
 
     /// @inheritdoc ICapChainlinkPoRAddressList
-    function getPoRAddressList(uint256 startIndex, uint256 endIndex) external view returns (string[] memory) {
+    function getPoRAddressList(uint256 startIndex, uint256 endIndex) external view returns (PoRInfo[] memory _infos) {
+        CapChainlinkPoRAddressListStorage storage $ = getCapChainlinkPoRAddressListStorage();
         if (startIndex > endIndex) {
-            return new string[](0);
+            return new PoRInfo[](0);
         }
 
         address[] memory addresses = _getcUSDAssets();
         endIndex = endIndex > addresses.length - 1 ? addresses.length - 1 : endIndex;
-        string[] memory stringAddresses = new string[](endIndex - startIndex + 1);
+        _infos = new PoRInfo[](endIndex - startIndex + 1);
         uint256 currIdx = startIndex;
         uint256 strAddrIdx = 0;
         while (currIdx <= endIndex) {
-            stringAddresses[strAddrIdx] = _toString(abi.encodePacked(addresses[currIdx]));
+            PoRInfo memory info = PoRInfo({
+                chain: "ethereum",
+                chainId: 1,
+                tokenSymbol: IERC20Metadata(addresses[currIdx]).symbol(),
+                tokenAddress: addresses[currIdx],
+                tokenDecimals: IERC20Metadata(addresses[currIdx]).decimals(),
+                tokenPriceOracle: $.tokenPriceOracles[addresses[currIdx]],
+                yourVaultAddress: address($.cusd)
+            });
+            _infos[strAddrIdx] = info;
             strAddrIdx++;
             currIdx++;
         }
-        return stringAddresses;
+        return _infos;
+    }
+
+    /// @inheritdoc ICapChainlinkPoRAddressList
+    function addTokenPriceOracle(address _token, address _oracle)
+        external
+        checkAccess(this.addTokenPriceOracle.selector)
+    {
+        CapChainlinkPoRAddressListStorage storage $ = getCapChainlinkPoRAddressListStorage();
+        $.tokenPriceOracles[_token] = _oracle;
     }
 
     /// @dev Get the list of assets supported by the vault
@@ -47,19 +81,6 @@ contract CapChainlinkPoRAddressList is ICapChainlinkPoRAddressList, CapChainlink
         return $.cusd.assets();
     }
 
-    /// @dev Convert bytes to string
-    /// @param data Bytes to convert
-    /// @return String representation of bytes
-    function _toString(bytes memory data) private pure returns (string memory) {
-        bytes memory alphabet = "0123456789abcdef";
-
-        bytes memory str = new bytes(2 + data.length * 2);
-        str[0] = "0";
-        str[1] = "x";
-        for (uint256 i = 0; i < data.length; i++) {
-            str[2 + i * 2] = alphabet[uint256(uint8(data[i] >> 4))];
-            str[3 + i * 2] = alphabet[uint256(uint8(data[i] & 0x0f))];
-        }
-        return string(str);
-    }
+    /// @inheritdoc UUPSUpgradeable
+    function _authorizeUpgrade(address newImplementation) internal override checkAccess(bytes4(0)) { }
 }
--- test/deploy/TestPoRAddressList.t.sol
+++ test/deploy/TestPoRAddressList.t.sol
@@ -1,28 +1,58 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import { CapChainlinkPoRAddressList } from "../../contracts/oracle/chainlink/CapChainlinkPoRAddressList.sol";
+import { AccessControl } from "../../contracts/access/AccessControl.sol";
+import { ProxyUtils } from "../../contracts/deploy/utils/ProxyUtils.sol";
+import {
+    CapChainlinkPoRAddressList,
+    ICapChainlinkPoRAddressList
+} from "../../contracts/oracle/chainlink/CapChainlinkPoRAddressList.sol";
 import { TestDeployer } from "./TestDeployer.sol";
 import { console } from "forge-std/console.sol";
 
 contract TestPoRAddressList is TestDeployer {
+    CapChainlinkPoRAddressList porAddressList;
+
     function setUp() public {
         _deployCapTestEnvironment();
         _initTestVaultLiquidity(usdVault);
         _initSymbioticVaultsLiquidity(env, 100);
+
+        address impl = address(new CapChainlinkPoRAddressList());
+        porAddressList = CapChainlinkPoRAddressList(_proxy(impl));
+
+        porAddressList.initialize(env.infra.accessControl, env.usdVault.capToken);
+
+        vm.startPrank(env.users.access_control_admin);
+        AccessControl(env.infra.accessControl).grantAccess(
+            ICapChainlinkPoRAddressList.addTokenPriceOracle.selector, address(porAddressList), env.users.deployer
+        );
+        vm.stopPrank();
     }
 
     function test_getPoRAddressListLength() public {
-        CapChainlinkPoRAddressList porAddressList = new CapChainlinkPoRAddressList(env.usdVault.capToken);
         assertEq(porAddressList.getPoRAddressListLength(), 3);
     }
 
     function test_getPoRAddressList() public {
-        CapChainlinkPoRAddressList porAddressList = new CapChainlinkPoRAddressList(env.usdVault.capToken);
         assertEq(porAddressList.getPoRAddressListLength(), 3);
-        string[] memory addresses = porAddressList.getPoRAddressList(0, 2);
-        console.log(addresses[0]);
-        console.log(addresses[1]);
-        console.log(addresses[2]);
+        ICapChainlinkPoRAddressList.PoRInfo[] memory addresses = porAddressList.getPoRAddressList(1, 2);
+        console.log(addresses[0].chain);
+        console.log(addresses[0].chainId);
+        console.log(addresses[0].tokenSymbol);
+        console.log(addresses[0].tokenAddress);
+        console.log(addresses[0].tokenDecimals);
+        console.log(addresses[0].tokenPriceOracle);
+        console.log(addresses[0].yourVaultAddress);
+    }
+
+    function test_addTokenPriceOracle() public {
+        /// create random address
+        address randomAddress = address(uint160(uint256(keccak256("random"))));
+        vm.startPrank(env.users.deployer);
+        porAddressList.addTokenPriceOracle(env.usdVault.assets[1], randomAddress);
+        vm.stopPrank();
+        ICapChainlinkPoRAddressList.PoRInfo[] memory addresses = porAddressList.getPoRAddressList(1, 2);
+        assertEq(addresses[0].tokenPriceOracle, randomAddress);
     }
 }
