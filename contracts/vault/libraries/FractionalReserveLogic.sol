db6080dc35a08f53d0be8164dac382e35d4051c5
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2025-08-18 16:54:03 +0100

    Issue 441
    On redeeming all Yearn vault shares, send any yield to the interest receiver to prevent an underflow

--- contracts/vault/libraries/FractionalReserveLogic.sol
+++ contracts/vault/libraries/FractionalReserveLogic.sol
@@ -96,6 +96,11 @@ library FractionalReserveLogic {
 
                 if (shares > vaultBalance) {
                     divestAmount = IERC4626($.vault[_asset]).redeem(vaultBalance, address(this), address(this));
+                    if (divestAmount > $.loaned[_asset]) {
+                        IERC20(_asset).safeTransfer($.interestReceiver, divestAmount - $.loaned[_asset]);
+                        emit FractionalReserveInterestRealized(_asset);
+                        divestAmount = $.loaned[_asset];
+                    }
                 } else {
                     IERC4626($.vault[_asset]).withdraw(divestAmount, address(this), address(this));
                 }
