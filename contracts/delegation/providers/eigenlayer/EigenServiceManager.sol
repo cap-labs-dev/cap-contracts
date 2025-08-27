9c07a795378107c4cdab7427b1930949302598a9
MirthFutures <MirthFutures@gmail.com>
2025-08-27 19:31:26 -0400

    reorder

--- contracts/delegation/providers/eigenlayer/EigenServiceManager.sol
+++ contracts/delegation/providers/eigenlayer/EigenServiceManager.sol
@@ -7,7 +7,6 @@ import { IOracle } from "../../../interfaces/IOracle.sol";
 import { EigenServiceManagerStorageUtils } from "../../../storage/EigenServiceManagerStorageUtils.sol";
 import { IAllocationManager } from "./interfaces/IAllocationManager.sol";
 import { IDelegationManager } from "./interfaces/IDelegationManager.sol";
-
 import { IRewardsCoordinator } from "./interfaces/IRewardsCoordinator.sol";
 import { IStrategy } from "./interfaces/IStrategy.sol";
 import { IStrategyManager } from "./interfaces/IStrategyManager.sol";
@@ -104,100 +103,6 @@ contract EigenServiceManager is IEigenServiceManager, UUPSUpgradeable, Access, E
     }
 
     /// @inheritdoc IEigenServiceManager
-    function slashableCollateral(address operator, uint256) external view returns (uint256) {
-        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
-        if ($.operatorToStrategy[operator] == address(0)) revert ZeroAddress();
-        return slashableCollateralByStrategy(operator, $.operatorToStrategy[operator]);
-    }
-
-    /// @notice Get the slashable collateral for a given operator and strategy
-    /// @param _operator The operator address
-    /// @param _strategy The strategy address
-    /// @return The slashable collateral
-    function slashableCollateralByStrategy(address _operator, address _strategy) private view returns (uint256) {
-        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
-        address collateralAddress = address(IStrategy(_strategy).underlyingToken());
-        uint8 decimals = IERC20Metadata(collateralAddress).decimals();
-        (uint256 collateralPrice,) = IOracle($.oracle).getPrice(collateralAddress);
-
-        uint256 collateral = _getSlashableShares(_operator);
-        uint256 collateralValue = collateral * collateralPrice / (10 ** decimals);
-
-        return collateralValue;
-    }
-
-    /// @inheritdoc IEigenServiceManager
-    function coverage(address operator) external view returns (uint256 delegation) {
-        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
-        address _strategy = $.operatorToStrategy[operator];
-        if (_strategy == address(0)) revert ZeroAddress();
-        address _oracle = $.oracle;
-        (delegation,) = coverageByStrategy(operator, _strategy, _oracle);
-    }
-
-    /// @notice Get the coverage for a given operator and strategy
-    /// @param _operator The operator address
-    /// @param _strategy The strategy address
-    /// @param _oracle The oracle address
-    /// @return collateralValue The collateral value
-    /// @return collateral The collateral
-    function coverageByStrategy(address _operator, address _strategy, address _oracle)
-        private
-        view
-        returns (uint256 collateralValue, uint256 collateral)
-    {
-        address collateralAddress = address(IStrategy(_strategy).underlyingToken());
-        uint8 decimals = IERC20Metadata(collateralAddress).decimals();
-        (uint256 collateralPrice,) = IOracle(_oracle).getPrice(collateralAddress);
-
-        collateral = _minimumSlashableStake(_operator, _strategy);
-        collateralValue = collateral * collateralPrice / (10 ** decimals);
-    }
-
-    /// @notice Get the slashable shares for a given operator and strategy
-    /// @param operator The operator address
-    /// @return The slashable shares of the operator
-    function _getSlashableShares(address operator) private view returns (uint256) {
-        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
-        address _strategy = $.operatorToStrategy[operator];
-        // Get the slashable shares for the operator/OperatorSet
-        uint256 slashableShares = _minimumSlashableStake(operator, _strategy);
-        // Get the shares in queue
-        uint256 sharesInQueue = _slashableSharesInQueue(operator, _strategy);
-        // Sum up the slashable shares and the shares in queue
-        uint256 totalSlashableShares = slashableShares + sharesInQueue;
-
-        return totalSlashableShares;
-    }
-
-    /// @notice Get the slashable shares in queue for withdrawal from a given operator and strategy
-    /// @param operator The operator address
-    /// @param strategy The strategy address
-    /// @return The slashable shares in queue for withdrawal
-    function _slashableSharesInQueue(address operator, address strategy) private view returns (uint256) {
-        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
-        return IDelegationManager($.delegationManager).getSlashableSharesInQueue(operator, strategy);
-    }
-
-    /// @notice Get the minimum slashable stake for a given operator and strategy
-    /// @param operator The operator address
-    /// @param strategy The strategy address
-    /// @return The minimum slashable stake
-    function _minimumSlashableStake(address operator, address strategy) private view returns (uint256) {
-        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
-        IAllocationManager.OperatorSet memory operatorSet =
-            IAllocationManager.OperatorSet({ avs: address(this), id: $.operatorSetIds[operator] });
-        address[] memory operators = new address[](1);
-        operators[0] = operator;
-        address[] memory strategies = new address[](1);
-        strategies[0] = strategy;
-        uint256[][] memory slashableShares = IAllocationManager($.allocationManager).getMinimumSlashableStake(
-            operatorSet, operators, strategies, uint32(block.number)
-        );
-        return slashableShares[0][0];
-    }
-
-    /// @inheritdoc IEigenServiceManager
     function slash(address _operator, address _recipient, uint256 _slashShare, uint48)
         external
         checkAccess(this.slash.selector)
@@ -214,34 +119,6 @@ contract EigenServiceManager is IEigenServiceManager, UUPSUpgradeable, Access, E
         _slashedCollateral.safeTransfer(_recipient, slashedAmount);
 
         emit Slash(_operator, _recipient, slashedAmount, uint48(block.timestamp));
-    }
-
-    /// @notice Slash the operator
-    /// @param _strategy The strategy address
-    /// @param _operator The operator address
-    /// @param _slashShare The slash share
-    function _slash(address _strategy, address _operator, uint256 _slashShare) private {
-        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
-        address[] memory strategies = new address[](1);
-        strategies[0] = _strategy;
-
-        uint256[] memory wadsToSlash = new uint256[](1);
-        wadsToSlash[0] = _slashShare;
-
-        IAllocationManager.SlashingParams memory slashingParams = IAllocationManager.SlashingParams({
-            operator: _operator,
-            operatorSetId: $.operatorSetIds[_operator],
-            strategies: strategies,
-            wadsToSlash: wadsToSlash,
-            description: "liquidation"
-        });
-
-        (uint256 slashId,) = IAllocationManager($.allocationManager).slashOperator(address(this), slashingParams);
-
-        IAllocationManager.OperatorSet memory operatorSet =
-            IAllocationManager.OperatorSet({ avs: address(this), id: $.operatorSetIds[_operator] });
-
-        IStrategyManager($.strategyManager).clearBurnOrRedistributableSharesByStrategy(operatorSet, slashId, _strategy);
     }
 
     /// @inheritdoc IEigenServiceManager
@@ -340,9 +217,53 @@ contract EigenServiceManager is IEigenServiceManager, UUPSUpgradeable, Access, E
         emit MinRewardAmountSet(_minRewardAmount);
     }
 
+    /// @inheritdoc IEigenServiceManager
+    function coverage(address operator) external view returns (uint256 delegation) {
+        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
+        address _strategy = $.operatorToStrategy[operator];
+        if (_strategy == address(0)) revert ZeroAddress();
+        address _oracle = $.oracle;
+        (delegation,) = coverageByStrategy(operator, _strategy, _oracle);
+    }
+
+    /// @inheritdoc IEigenServiceManager
+    function slashableCollateral(address operator, uint256) external view returns (uint256) {
+        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
+        if ($.operatorToStrategy[operator] == address(0)) revert ZeroAddress();
+        return slashableCollateralByStrategy(operator, $.operatorToStrategy[operator]);
+    }
+
+    /// @notice Slash the operator
+    /// @param _strategy The strategy address
+    /// @param _operator The operator address
+    /// @param _slashShare The slash share
+    function _slash(address _strategy, address _operator, uint256 _slashShare) private {
+        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
+        address[] memory strategies = new address[](1);
+        strategies[0] = _strategy;
+
+        uint256[] memory wadsToSlash = new uint256[](1);
+        wadsToSlash[0] = _slashShare;
+
+        IAllocationManager.SlashingParams memory slashingParams = IAllocationManager.SlashingParams({
+            operator: _operator,
+            operatorSetId: $.operatorSetIds[_operator],
+            strategies: strategies,
+            wadsToSlash: wadsToSlash,
+            description: "liquidation"
+        });
+
+        (uint256 slashId,) = IAllocationManager($.allocationManager).slashOperator(address(this), slashingParams);
+
+        IAllocationManager.OperatorSet memory operatorSet =
+            IAllocationManager.OperatorSet({ avs: address(this), id: $.operatorSetIds[_operator] });
+
+        IStrategyManager($.strategyManager).clearBurnOrRedistributableSharesByStrategy(operatorSet, slashId, _strategy);
+    }
+
     /// @notice Create a rewards submission for the AVS
     /// @param rewardsSubmissions The rewards submissions being created
-    function _createAVSRewardsSubmission(IRewardsCoordinator.RewardsSubmission[] memory rewardsSubmissions) internal {
+    function _createAVSRewardsSubmission(IRewardsCoordinator.RewardsSubmission[] memory rewardsSubmissions) private {
         EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
         IRewardsCoordinator($.rewardsCoordinator).createAVSRewardsSubmission(rewardsSubmissions);
     }
@@ -350,10 +271,88 @@ contract EigenServiceManager is IEigenServiceManager, UUPSUpgradeable, Access, E
     /// @notice Check if the token has enough allowance for the spender
     /// @param token The token to check
     /// @param spender The spender to check
-    function _checkApproval(address token, address spender) internal {
+    function _checkApproval(address token, address spender) private {
         if (IERC20(token).allowance(spender, address(this)) == 0) {
             IERC20(token).forceApprove(spender, type(uint256).max);
         }
+    }
+
+    /// @notice Get the slashable collateral for a given operator and strategy
+    /// @param _operator The operator address
+    /// @param _strategy The strategy address
+    /// @return The slashable collateral
+    function slashableCollateralByStrategy(address _operator, address _strategy) private view returns (uint256) {
+        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
+        address collateralAddress = address(IStrategy(_strategy).underlyingToken());
+        uint8 decimals = IERC20Metadata(collateralAddress).decimals();
+        (uint256 collateralPrice,) = IOracle($.oracle).getPrice(collateralAddress);
+
+        uint256 collateral = _getSlashableShares(_operator);
+        uint256 collateralValue = collateral * collateralPrice / (10 ** decimals);
+
+        return collateralValue;
+    }
+
+    /// @notice Get the coverage for a given operator and strategy
+    /// @param _operator The operator address
+    /// @param _strategy The strategy address
+    /// @param _oracle The oracle address
+    /// @return collateralValue The collateral value
+    /// @return collateral The collateral
+    function coverageByStrategy(address _operator, address _strategy, address _oracle)
+        private
+        view
+        returns (uint256 collateralValue, uint256 collateral)
+    {
+        address collateralAddress = address(IStrategy(_strategy).underlyingToken());
+        uint8 decimals = IERC20Metadata(collateralAddress).decimals();
+        (uint256 collateralPrice,) = IOracle(_oracle).getPrice(collateralAddress);
+
+        collateral = _minimumSlashableStake(_operator, _strategy);
+        collateralValue = collateral * collateralPrice / (10 ** decimals);
+    }
+
+    /// @notice Get the slashable shares for a given operator and strategy
+    /// @param operator The operator address
+    /// @return The slashable shares of the operator
+    function _getSlashableShares(address operator) private view returns (uint256) {
+        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
+        address _strategy = $.operatorToStrategy[operator];
+        // Get the slashable shares for the operator/OperatorSet
+        uint256 slashableShares = _minimumSlashableStake(operator, _strategy);
+        // Get the shares in queue
+        uint256 sharesInQueue = _slashableSharesInQueue(operator, _strategy);
+        // Sum up the slashable shares and the shares in queue
+        uint256 totalSlashableShares = slashableShares + sharesInQueue;
+
+        return totalSlashableShares;
+    }
+
+    /// @notice Get the slashable shares in queue for withdrawal from a given operator and strategy
+    /// @param operator The operator address
+    /// @param strategy The strategy address
+    /// @return The slashable shares in queue for withdrawal
+    function _slashableSharesInQueue(address operator, address strategy) private view returns (uint256) {
+        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
+        return IDelegationManager($.delegationManager).getSlashableSharesInQueue(operator, strategy);
+    }
+
+    /// @notice Get the minimum slashable stake for a given operator and strategy
+    /// @param operator The operator address
+    /// @param strategy The strategy address
+    /// @return The minimum slashable stake
+    function _minimumSlashableStake(address operator, address strategy) private view returns (uint256) {
+        EigenServiceManagerStorage storage $ = getEigenServiceManagerStorage();
+        IAllocationManager.OperatorSet memory operatorSet =
+            IAllocationManager.OperatorSet({ avs: address(this), id: $.operatorSetIds[operator] });
+        address[] memory operators = new address[](1);
+        operators[0] = operator;
+        address[] memory strategies = new address[](1);
+        strategies[0] = strategy;
+        uint256[][] memory slashableShares = IAllocationManager($.allocationManager).getMinimumSlashableStake(
+            operatorSet, operators, strategies, uint32(block.number)
+        );
+        return slashableShares[0][0];
     }
 
     /// @inheritdoc UUPSUpgradeable
