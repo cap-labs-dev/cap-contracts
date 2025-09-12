25f3fc8a4aa186070381c7d19c170e524a815c5a
MirthFutures <MirthFutures@gmail.com>
2025-09-12 10:56:17 -0400

    Update cUSD Implementation for view functionality

--- script/DeployContract.s.sol
+++ script/DeployContract.s.sol
@@ -1,23 +1,15 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
-import { CapChainlinkPoRAddressList } from "../contracts/oracle/chainlink/CapChainlinkPoRAddressList.sol";
+import { CapToken } from "../contracts/token/CapToken.sol";
 import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
 
 contract DeployContract is Script {
     function run() external {
         vm.startBroadcast();
-        CapChainlinkPoRAddressList chainlinkPoRAddressList = new CapChainlinkPoRAddressList();
-        console.log("CapChainlinkPoRAddressList deployed to:", address(chainlinkPoRAddressList));
+        CapToken capToken = new CapToken();
+        console.log("CapToken deployed to:", address(capToken));
         vm.stopBroadcast();
-        /* address vaultConfigurator = 0x29300b1d3150B4E2b12fE80BE72f365E200441EC;
-        address burnerRouterFactory = 0x99F2B89fB3C363fBafD8d826E5AA77b28bAB70a0;
-        address defaultStakerRewardsFactory = 0xFEB871581C2ab2e1EEe6f7dDC7e6246cFa087A23;
-        address middleware = 0x8C9140Fe6650E56A0A07E86455D745f8F7843B6D;
-        CapSymbioticVaultFactory factory = new CapSymbioticVaultFactory(
-            vaultConfigurator, burnerRouterFactory, defaultStakerRewardsFactory, middleware
-        );
-        console.log("CapSymbioticVaultFactory deployed to:", address(factory));*/
     }
 }
