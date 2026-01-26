14c14d62b5b5fbce67bd0bd9f5bb777dc0aa4284
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2026-01-26 22:12:01 +0000

    Add Predicate to ValidationHook

--- contracts/ico/ValidationHook.sol
+++ contracts/ico/ValidationHook.sol
@@ -3,23 +3,32 @@ pragma solidity ^0.8.28;
 
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import { Attestation } from "@predicate/interfaces/IPredicateRegistry.sol";
+import { PredicateClient } from "@predicate/mixins/PredicateClient.sol";
 
 import { Access } from "../access/Access.sol";
-import { IValidationHook } from "../interfaces/IValidationHook.sol";
+import { IPredicateClient, IValidationHook } from "../interfaces/IValidationHook.sol";
 import { ValidationHookStorageUtils } from "../storage/ValidationHookStorageUtils.sol";
 
 /// @title ValidationHook
 /// @author kexley, Cap Labs
 /// @notice Validation hook using a soulbound ERC721 token and a time gate
-/// @dev This hook validates that the sender is the owner of a specific ERC721 token or the time gate has passed
-contract ValidationHook is IValidationHook, UUPSUpgradeable, Access, ValidationHookStorageUtils {
+/// @dev This hook validates that the sender is the owner of a specific ERC721 token or the time gate has passed.
+/// The auction address is set after initialization since this contract address is used in the auction constructor.
+contract ValidationHook is IValidationHook, UUPSUpgradeable, PredicateClient, Access, ValidationHookStorageUtils {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
     /// @inheritdoc IValidationHook
-    function initialize(address _accessControl, address _token, uint256 _gate) external initializer {
+    function initialize(
+        address _accessControl,
+        address _token,
+        uint256 _gate,
+        address _registry,
+        string memory _policyID
+    ) external initializer {
         if (_accessControl == address(0)) revert ZeroAddressNotValid();
         __Access_init(_accessControl);
 
@@ -30,17 +39,39 @@ contract ValidationHook is IValidationHook, UUPSUpgradeable, Access, ValidationH
         if (_gate < block.timestamp) revert InvalidGate();
         $.gate = _gate;
 
+        _initPredicateClient(_registry, _policyID);
+
         __UUPSUpgradeable_init();
     }
 
     /// @inheritdoc IValidationHook
-    function validate(uint256, uint128, address owner, address sender, bytes calldata) external view {
-        if (sender != owner) revert SenderMustBeOwner();
-
+    function validate(uint256, uint128, address _owner, address _sender, bytes calldata _hookData) external {
         ValidationHookStorage storage $ = getValidationHookStorage();
+
+        // attestations can be wasted by third parties if not routed through the auction
+        if (msg.sender != $.auction) revert CallerMustBeAuction();
+
+        // the sender must be the owner of the bid
+        if (_sender != _owner) revert SenderMustBeOwner();
+
+        // if time gate is not passed, the sender must be the owner of the ERC721 token
         if (block.timestamp < $.gate) {
-            if (IERC721($.token).balanceOf(owner) == 0) revert NotOwnerOfERC721Token();
+            if (IERC721($.token).balanceOf(_sender) == 0) revert NotOwnerOfERC721Token();
         }
+
+        // attestation is decoded from the hook data
+        Attestation memory attestation = abi.decode(_hookData, (Attestation));
+
+        // empty encodedSigAndArgs means only the _sender address is validated for compliance
+        if (!_authorizeTransaction(attestation, hex"", _sender, 0)) revert InvalidAttestation();
+
+        emit AttestationValidated(_sender, attestation.uuid);
+    }
+
+    /// @inheritdoc IValidationHook
+    function setAuction(address _auction) external checkAccess(this.setAuction.selector) {
+        if (_auction == address(0)) revert ZeroAddressNotValid();
+        getValidationHookStorage().auction = _auction;
     }
 
     /// @inheritdoc IValidationHook
@@ -52,6 +83,31 @@ contract ValidationHook is IValidationHook, UUPSUpgradeable, Access, ValidationH
     /// @inheritdoc IValidationHook
     function setGate(uint256 _gate) external checkAccess(this.setGate.selector) {
         getValidationHookStorage().gate = _gate;
+    }
+
+    /// @inheritdoc IPredicateClient
+    function setRegistry(address _registry) external checkAccess(this.setRegistry.selector) {
+        _setRegistry(_registry);
+    }
+
+    /// @inheritdoc IPredicateClient
+    function setPolicyID(string memory _policyID) external checkAccess(this.setPolicyID.selector) {
+        _setPolicyID(_policyID);
+    }
+
+    /// @inheritdoc IValidationHook
+    function auction() external view returns (address) {
+        return getValidationHookStorage().auction;
+    }
+
+    /// @inheritdoc IValidationHook
+    function token() external view returns (address) {
+        return getValidationHookStorage().token;
+    }
+
+    /// @inheritdoc IValidationHook
+    function gate() external view returns (uint256) {
+        return getValidationHookStorage().gate;
     }
 
     /// @inheritdoc UUPSUpgradeable
--- contracts/interfaces/IValidationHook.sol
+++ contracts/interfaces/IValidationHook.sol
@@ -1,18 +1,28 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
+import { IPredicateClient } from "@predicate/interfaces/IPredicateClient.sol";
+
 /// @title IValidationHook
 /// @notice Interface for the validation hook
 /// @author kexley, Cap Labs
-interface IValidationHook {
+interface IValidationHook is IPredicateClient {
     /// @custom:storage-location erc7201:cap.storage.ValidationHook
     /// @dev Validation hook storage
+    /// @param auction Auction address
     /// @param token ERC721 token address
     /// @param gateUntil Time gate timestamp
     struct ValidationHookStorage {
+        address auction;
         address token;
         uint256 gate;
     }
+
+    /// @notice Error thrown when the caller is not the auction
+    error CallerMustBeAuction();
+
+    /// @notice Error thrown when the attestation is invalid
+    error InvalidAttestation();
 
     /// @notice Error thrown when the gate timestamp is invalid
     error InvalidGate();
@@ -26,11 +36,24 @@ interface IValidationHook {
     /// @notice Error thrown when the zero address is not valid
     error ZeroAddressNotValid();
 
+    /// @notice Event emitted when an attestation is validated
+    /// @param sender The sender of the transaction
+    /// @param uuid The UUID of the attestation
+    event AttestationValidated(address indexed sender, string uuid);
+
     /// @notice Initialize the validation hook
     /// @param _accessControl Access control address
     /// @param _token ERC721 token address
     /// @param _gate Time gate timestamp
-    function initialize(address _accessControl, address _token, uint256 _gate) external;
+    /// @param _registry Predicate registry address
+    /// @param _policyID Predicate policy ID
+    function initialize(
+        address _accessControl,
+        address _token,
+        uint256 _gate,
+        address _registry,
+        string memory _policyID
+    ) external;
 
     /// @notice Validate a bid
     /// @dev MUST revert if the bid is invalid
@@ -42,6 +65,11 @@ interface IValidationHook {
     function validate(uint256 _maxPrice, uint128 _amount, address _owner, address _sender, bytes calldata _hookData)
         external;
 
+    /// @notice Set the auction address
+    /// @dev This function can be called by the admin to change the auction address
+    /// @param _auction Auction address
+    function setAuction(address _auction) external;
+
     /// @notice Set the ERC721 token address
     /// @dev This function can be called by the admin to change the required ERC721 token address
     /// @param _token ERC721 token address
@@ -51,4 +79,16 @@ interface IValidationHook {
     /// @dev This function can be called by the admin to change the time gate, even to the past to allow for immediate validation
     /// @param _gate Time gate timestamp
     function setGate(uint256 _gate) external;
+
+    /// @notice Get the auction address
+    /// @return . The auction address
+    function auction() external view returns (address);
+
+    /// @notice Get the ERC721 token address
+    /// @return . The ERC721 token address
+    function token() external view returns (address);
+
+    /// @notice Get the time gate timestamp
+    /// @return . The time gate timestamp
+    function gate() external view returns (uint256);
 }
